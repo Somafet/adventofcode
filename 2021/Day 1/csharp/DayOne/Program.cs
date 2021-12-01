de411fb1e4f534ff4dfab22c36d91ebdd763a661
Soma Somorjai <somorjais@cardiff.ac.uk>
2021-12-01 23:40:50 +0100

    Day1/A C#

---
+++ 2021/Day 1/csharp/DayOne/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var linesAsInt = File.ReadAllLines("./data.input").Select(int.Parse);
+
+int? prevNum = null;
+var totalIncreases = 0;
+
+foreach (var num in linesAsInt)
+{
+    totalIncreases += num > prevNum ? 1 : 0;
+    prevNum = num;
+}
+
+Console.WriteLine(totalIncreases);
