f962de327632981a332d9290e7578528af8a0ac3
Soma Somorjai <somorjais@cardiff.ac.uk>
2021-12-02 11:09:29 +0100

    Day 2 C#

---
+++ 2021/Day 2/csharp/DayTwo/Program.cs
@@ -0,0 +1,39 @@
+﻿
+var horizontal = 0;
+var depth = 0;
+var aim = 0;
+
+var actionTableA = new Dictionary<string, Action<int>>()
+{
+    { "forward", (value) => horizontal += value },
+    { "down", (value) => depth += value },
+    { "up", (value) => depth -= value },
+};
+
+var actionTableB = new Dictionary<string, Action<int>>()
+{
+    { "forward", (value) =>
+        {
+            horizontal += value;
+            depth += aim * value;
+        }
+    },
+    { "down", (value) => aim += value },
+    { "up", (value) => aim -= value },
+};
+
+var linesList = File.ReadAllLines("data.input")
+    .Select(line => line.Split(' '))
+    .ToList();
+
+// Day 1 / A
+linesList.ForEach(line => actionTableA[line[0]](int.Parse(line[1])));
+
+Console.WriteLine(horizontal * depth);
+
+// Day 1 / B
+horizontal = 0;
+depth = 0;
+linesList.ForEach(line => actionTableB[line[0]](int.Parse(line[1])));
+
+Console.WriteLine(horizontal * depth);
